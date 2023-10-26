3841f79f3aeaf9f9b5b32319d185a9c86be41833
Horacio <oicaroh64@gmail.com>
2023-10-26 13:59:10 -0300

    Checking if logged

--- app/src/main/java/com/example/jumppark/ui/SplashScreenFragment.kt
+++ app/src/main/java/com/example/jumppark/ui/SplashScreenFragment.kt
@@ -1,5 +1,6 @@
 package com.example.jumppark.ui
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -9,9 +10,14 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.jumppark.R
 import com.example.jumppark.databinding.FragmentSplashScreenBinding
+import com.example.jumppark.ui.uiUtils.SharedPreferencesKeys
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SplashScreenFragment : BaseFragment() {
-
+    @Inject
+    lateinit var sharedPreferences: SharedPreferences
     private lateinit var binding: FragmentSplashScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,9 +34,21 @@ class SplashScreenFragment : BaseFragment() {
         binding = FragmentSplashScreenBinding.bind(view)
 
         Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            checkIfLogged()
+        }, 3000)
+    }
+
+    private fun checkIfLogged() {
+        if (sharedPreferences.getString(
+                "${SharedPreferencesKeys.token}",
+                ""
+            )?.length ?: 0 > 1
+        ) {
+            findNavController().navigate(SplashScreenFragmentDirections.actionSplashScreenFragmentToHomeFragment())
+        } else {
             findNavController()
                 .navigate(SplashScreenFragmentDirections.actionSplashScreenFragmentToLoginFragment())
-        }, 3000)
+        }
     }
 
 
